bc52d819121e753f1a70787db83f11ef72024cb5
Muhammad Amirul Bin Safiee <amirulsafiee@yahoo.com.sg>
2018-10-17 16:07:26 +0800

    Complete player and camera movement script, added enemy AI prototype with another scene for testing

---
+++ Assets/Scripts/BulletSpeed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpeed : MonoBehaviour
+{
+    private float speed = 20f;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.localPosition += Vector3.forward * speed * Time.deltaTime;
+	}
+}
