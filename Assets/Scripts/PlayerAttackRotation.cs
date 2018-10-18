503d34d471a5c080bafa52326ec827c3deb1b0c2
Muhammad Amirul Bin Safiee <amirulsafiee@yahoo.com.sg>
2018-10-18 17:17:56 +0800

    Added assets and scripts for joystick movement and shooting

---
+++ Assets/Scripts/PlayerAttackRotation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackRotation : MonoBehaviour
+{
+    protected AttackRotationJoystick joystick;
+
+    public GameObject bullet;
+    public Transform bulletSpawn;
+
+    private float release;
+    private float overheating;
+    private bool overheat;
+
+    // Use this for initialization
+    void Start ()
+    {
+        joystick = FindObjectOfType<AttackRotationJoystick>();
+        overheat = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        overheating -= Time.deltaTime;
+
+        if (joystick.Pressed && overheat == false)
+        {
+            release += Time.deltaTime;
+
+            if (release >= 0.1f)
+            {
+                SpawnBullet();
+                release = 0.0f;
+                overheating += 0.4f;
+            }
+        }
+
+        if (overheating < 0.0f)
+        {
+            overheating = 0.0f;
+        }
+
+        if (overheating >= 2.0f)
+        {
+            overheat = true;
+        }
+
+        if (overheating < 2.0f)
+        {
+            overheat = false;
+        }
+    }
+
+    void SpawnBullet()
+    {
+        Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+    }
+}
---
+++ Assets/Scripts/PlayerJoyStickMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoyStickMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float rotationSpeed = 100.0f;
+
+    protected Joystick joystick;
+
+	// Use this for initialization
+	void Start ()
+    {
+        joystick = FindObjectOfType<Joystick>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //var rigidbody = GetComponent<Rigidbody>();
+
+        //rigidbody.velocity = new Vector3(joystick.Horizontal * 5f, rigidbody.velocity.y, joystick.Vertical * 5f);
+
+        // Move translation along the object's z-axis
+        transform.position += new Vector3(joystick.Horizontal * speed * Time.deltaTime, 0, joystick.Vertical * speed * Time.deltaTime);
+
+        // Rotate around our y-axis
+    }
+}
